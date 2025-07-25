a579ded6698e36842b8dc8d0190106ef0e054706
MikeyMiami <165108210+MikeyMiami@users.noreply.github.com>
2025-07-25 14:38:07 -0400

    Update googleSheets.js

--- utils/googleSheets.js
+++ utils/googleSheets.js
@@ -3,9 +3,15 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 async function getSheetClient() {
   const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
 
+  // Decode private key safely from environment variable
+  let decodedPrivateKey = process.env.GOOGLE_PRIVATE_KEY;
+  if (decodedPrivateKey.includes('\\n')) {
+    decodedPrivateKey = decodedPrivateKey.replace(/\\n/g, '\n');
+  }
+
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: decodedPrivateKey,
   });
 
   await doc.loadInfo();
