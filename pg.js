a8461479503009fb33482e1f18ef943aa1d06279
MikeyMiami <165108210+MikeyMiami@users.noreply.github.com>
2025-07-26 13:55:26 -0400

    Create pg.js

---
+++ pg.js
@@ -0,0 +1,15 @@
+// pg.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = pool;
